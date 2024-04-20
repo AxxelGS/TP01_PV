80fc509861c23962a8e9cf437ca901a93fbb1672
gabri <gabri@DESKTOP-D643OE0>
2024-04-20 11:59:07 -0300

    Ejercicio11 Resuelto

---
+++ TP01SanchezAxel/src/ar/edu/unju/fi/ejercicio11/main.java
@@ -0,0 +1,14 @@
+package ar.edu.unju.fi.ejercicio11;
+
+public class main {
+
+	public static void main(String[] args) {
+		int j;
+		int i=2;
+		for (j=40;j>0;j--) {
+			System.out.println(j*i);
+			i++;
+		}
+	}
+}
+//Resolucion punto 11
